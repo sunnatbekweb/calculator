5cfec4172edf3b925c8b7dfa4d881778911391b4
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-09-18 23:50:40 +0500

    cleared

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import Buttons from './Ui/Buttons';
-import Button2 from './Ui/Button2';
-
 function App() {
   return (
     <>
@@ -10,32 +7,13 @@ function App() {
             <h2>0</h2>
           </div>
           <div className="calculator_buttons">
-            <div className='calculator_button'>
-              <Buttons text={"+"} />
-              <Buttons text={"-"} />
-              <Buttons text={"x"} />
-              <Buttons text={"7"} />
-              <Buttons text={"8"} />
-              <Buttons text={"9"} />
-              <Buttons text={"4"} />
-              <Buttons text={"5"} />
-              <Buttons text={"6"} />
-              <Buttons text={"1"} />
-              <Buttons text={"2"} />
-              <Buttons text={"3"} />
-              <button className='null_btn'>0</button>
-              <Buttons text={"."} />
-              <Buttons text={"AC"} />
-            </div>
-            <div>
-              <Buttons text={'%'} />
-              <Button2 />
-            </div>
+            <div className="calculator_button"></div>
+            <div></div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;

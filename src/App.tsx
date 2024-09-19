5cfc0b4bd1555dce53b01c2307ea0eced8e9368c
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-09-19 20:57:03 +0500

    created buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,27 @@ function App() {
         <div className="calculator">
           <h2 className="value">2+2</h2>
           <h1 className="result">=4</h1>
+
           <div className="buttons_wrapper">
-    
+            <button>C</button>
+            <button>%</button>
+            <button>/</button>
+            <button>icon</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>X</button>
+            <button>5</button>
+            <button>6</button>
+            <button>4</button>
+            <button>-</button>
+            <button>3</button>
+            <button>2</button>
+            <button>1</button>
+            <button>/</button>
+            <button>0</button>
+            <button>.</button>
+            <button className="equal">=</button>
           </div>
         </div>
       </div>

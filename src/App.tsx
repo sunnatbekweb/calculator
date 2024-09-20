9f970343f61dcdb5ba111afa88fff5c98c237575
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-09-20 21:48:19 +0500

    created fuction

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 function App() {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget.dataset.value);
+    
+  };
+
   return (
     <>
       <div className="container">
@@ -7,24 +12,24 @@ function App() {
           <h1 className="result">=4</h1>
 
           <div className="buttons_wrapper">
-            <button>C</button>
-            <button>%</button>
-            <button>/</button>
-            <button>icon</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>X</button>
-            <button>5</button>
-            <button>6</button>
-            <button>4</button>
-            <button>-</button>
-            <button>3</button>
-            <button>2</button>
-            <button>1</button>
-            <button>/</button>
-            <button>0</button>
-            <button>.</button>
+            <button data-value="C" onClick={handleClick}>C</button>
+            <button data-value="%" onClick={handleClick}>%</button>
+            <button data-value="/" onClick={handleClick}>/</button>
+            <button data-value="de onClick={handleClick}l">icon</button>
+            <button data-value="7" onClick={handleClick}>7</button>
+            <button data-value="8" onClick={handleClick}>8</button>
+            <button data-value="9" onClick={handleClick}>9</button>
+            <button data-value="x" onClick={handleClick}>X</button>
+            <button data-value="5" onClick={handleClick}>5</button>
+            <button data-value="6" onClick={handleClick}>6</button>
+            <button data-value="4" onClick={handleClick}>4</button>
+            <button data-value="-" onClick={handleClick}>-</button>
+            <button data-value="3" onClick={handleClick}>3</button>
+            <button data-value="2" onClick={handleClick}>2</button>
+            <button data-value="1" onClick={handleClick}>1</button>
+            <button data-value="+" onClick={handleClick}>+</button>
+            <button data-value="0" onClick={handleClick}>0</button>
+            <button data-value="." onClick={handleClick}>.</button>
             <button className="equal">=</button>
           </div>
         </div>
